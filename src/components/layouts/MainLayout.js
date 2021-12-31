106edb28f42d18b2697a27e61686fa1fceba4279
Dakup Nengak <nengakdakup@gmail.com>
2021-12-31 03:30:54 +0100

    Configured MainLayout Component

--- src/components/layouts/MainLayout.js
+++ src/components/layouts/MainLayout.js
@@ -2,9 +2,9 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import './MainLayout.scss';
 
-export default function MainLayout({children}){
+export default function MainLayout({fixedHeight, children}){
     return (
-        <Container fluid>
+        <Container fluid className={['main_layout', fixedHeight? 'fixed_height--100':'']} >
             {children}
         </Container>
     )
--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
-
+import {Row, Col} from "react-bootstrap"
+import MainLayout from "../../components/layouts/MainLayout";
 import CustomButton from "../../components/Button/Button";
 
 export default function Welcome () {
     return (
-        <div>
-            <h2>Welcomeeee</h2>
+        <MainLayout fixedHeight>
+            <Row>
+                <Col>
+                    <h2>Welcomeeee</h2>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                </Col>
+            </Row>
+            
             <CustomButton title={'Welcome'} variant={'danger'}/>
-        </div>
+        </MainLayout>
     )
 }
